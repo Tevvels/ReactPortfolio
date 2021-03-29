e821f22876a7d7a841233212e3dbc05665eebdc8
chris watkins <chrisbwatkins@gmail.com>
2021-03-28 22:10:40 -0600

    gcommitting more styling to the project.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 
 import React from 'react';
 import "./style.css"
+import '../../pages/style.css'
 
 
 
 function Card(props) {
     return (
         <>
-            <a className={`card-anchor`} href={props.url}>
-                <div className={`card`} >
+            <a className={`card-anchor }`} href={props.url}>
+                <div className={`card ${props.classes}`} >
                     <h3 className={`card-name`}>{props.name}</h3>
                     <img className={`card-img`} src={props.image} />
                     <p className={`card-desc`}>{props.desc}</p>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,6 +7,7 @@ import daBurger from '../images/DaBurger.png'
 import one from '../images/one.png'
 import day_planner from '../images/day_planner_01.PNG'
 import "../components/Container/projects.css"
+import "./style.css"
 
 function portfolio(){
     return(
@@ -15,14 +16,19 @@ function portfolio(){
     
         <Container className={`container-projects`}
         
-            main={<Card name={'Brawndo'} image={brawndo}
-            url={"https://brawndoplant.herokuapp.com/garden"} desc={" An app that helps plants grow" } />}
+            main={<Card classes={`project-1`} name={'Brawndo'} image={brawndo}
+                url={"https://brawndoplant.herokuapp.com/garden"} desc={" An app that helps plants grow" } />}
 
-            main2={<Card name={'Code Quiz'} url={"httpshttps://brawndoplant.herokuapp.com/garden"} image={codequiz} desc={" An app that helps plants grow" } />}  
-           
-            main3={<Card name={'Daburger'} url={"https://arcane-headland-91349.herokuapp.com/"} image={daBurger} desc={" An app that helps plants grow"} /> } 
-            main4={<Card name={'Deep Vibrations'} url={"https://arcane-headland-91349.herokuapp.com/"} image={one} desc={" An app that helps plants grow"} /> } 
-            main6={<Card name={'Brawndo'} url={"https://brawndoplant.herokuapp.com/garden"} image={brawndo} desc={" An app that helps plants grow"} /> } 
+            main2={<Card classes={`project-2`} name={'Code Quiz'} url={"httpshttps://brawndoplant.herokuapp.com/garden"} 
+            image={codequiz} desc={" An app that helps plants grow" } />}  
+            main3={<Card classes={`project-3`} name={'Daburger'} url={"https://arcane-headland-91349.herokuapp.com/"} 
+            image={daBurger} desc={" An app that helps plants grow"} /> } 
+            main4={<Card classes={`project-4`} name={'Deep Vibrations'} url={"https://arcane-headland-91349.herokuapp.com/"} 
+            image={one} desc={" An app that helps plants grow"} /> } 
+            main5={<Card classes={`project-5`} name={'Brawndo'} url={"https://brawndoplant.herokuapp.com/garden"} 
+            image={brawndo} desc={" An app that helps plants grow"} /> } 
+            main6={<Card classes={`project-6`} name={'Brawndo'} url={"https://brawndoplant.herokuapp.com/garden"} 
+            image={brawndo} desc={" An app that helps plants grow"} /> } 
             
         />
 
